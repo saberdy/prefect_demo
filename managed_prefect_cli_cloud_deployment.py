77bd96f33afd86b7c1f6f7d1187bf3c1342cb897
saber <sabergit@proton.me>
2024-01-16 01:08:28 +0100

    replace gitlab private repo name/owner with repository id

--- managed_prefect_cli_cloud_deployment.py
+++ managed_prefect_cli_cloud_deployment.py
@@ -3,16 +3,16 @@ from prefect import flow
 
 
 @flow(log_prints=True)
-def get_repo_info(repo_name: str = "saberdy/prefect_demo"):
+def get_repo_info(repo_id: str = "53809079"):
     """ select a personal repository as default argument in the function
     definition.
     """
 
-    url = f"https://gitlab.com/api/v4/projects/{repo_name}"
+    url = f"https://gitlab.com/api/v4/projects/{repo_id}"
     response = httpx.get(url)
     response.raise_for_status()
     repo = response.json()
-    print(f"{repo_name} repository statistics 🤓:")
+    print(f"{repo_id} repository statistics 🤓:")
     print(f"Stars 🌠 : {repo['stargazers_count']}")
     print(f"Forks 🍴 : {repo['forks_count']}")
 
