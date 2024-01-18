548dbdfa0498e7f005eceaabe009c66b2bd01295
saber <sabergit@proton.me>
2024-01-18 17:25:14 +0100

    update the flow to a dummy flow and code snippet to use gitlab blocks

--- load_from_storage.py
+++ load_from_storage.py
@@ -1,20 +1,40 @@
+from datetime import datetime
 from prefect import flow
-from prefect.runner.storage import GitRepository
-from prefect.blocks.system import Secret
+# from prefect.runner.storage import GitRepository
+# from prefect.runner.storage import GitLabRepository
+from prefect_gitlab.repositories import GitLabRepository
+# from prefect.blocks.system import Secret
+from prefect_gitlab import GitLabCredentials
 
-my_flow = flow.from_source(
-    source=GitRepository(
-        # url="https://gitlab.com/saberdy/prefect_demo",
-        url="https://gitlab.com/api/v4/projects/53809079",
-        # branch="master",
-        credentials={
-            # "username": "saberdy",
-            "access_token": Secret.load("prefect-demo").get(),
-            # "branch": "master"
-            }
-       ),
-    entrypoint="repo_info.py:get_repo_info"
-   )
+gitlab_credentials_block = GitLabCredentials.load("gl-prefect-block")
+gitlab_repository_block = GitLabRepository.load("my-private-gitlab-block")
+
+
+@flow(retries=3, retry_delay_seconds=5, log_prints=True)
+def dummy_flow(date: datetime = datetime.now()):
+    print(f"It was {date.strftime('%A')} on {date.isoformat()}")
+
+
+my_flow = dummy_flow.from_source(
+    source=gitlab_repository_block,
+    entrypoint="load_from_storage.py:dummy_flow"
+)
+# my_flow = flow.from_source(
+#     source=gitlab_repository_block,
+#     # GitRepository(
+#     # url="https://gitlab.com/saberdy/prefect_demo.git",
+#     # url=gitlab_credentials_block,
+#     # url="https://gitlab.com/api/v4/projects/53809079",
+#     # branch="master",
+#     # credentials={"username": "saberdy",
+#     #             "access_token": gitlab_credentials_block},
+#     # credentials={
+#     #     "username": "saberdy",
+#     #     "access_token": Secret.load("prefect-demo").get(),
+#     #     # "branch": "master"
+#     entrypoint="repo_info.py:get_repo_info"
+#    )
 
 if __name__ == "__main__":
+    # dummy_flow()
     my_flow()
