1fc8d688b6ea7382d951516bd13a43205b53c1da
saber <sabergit@proton.me>
2024-01-19 16:50:58 +0100

    clean up the load from storage and fix typos for dummy flow

--- load_from_storage.py
+++ load_from_storage.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 from prefect import flow
-# from prefect.runner.storage import GitRepository
-# from prefect.runner.storage import GitLabRepository
 
 @flow(retries=3, retry_delay_seconds=5, log_prints=True)
 def dummy_flow(date: datetime = datetime.now()):
@@ -9,24 +7,10 @@ def dummy_flow(date: datetime = datetime.now()):
 
 
 my_flow = dummy_flow.from_source(
-    source="https://github.com/saberdy/prefect_demo"
+    source="https://github.com/saberdy/prefect_demo",
     entrypoint="load_from_storage.py:dummy_flow"
 )
-# my_flow = flow.from_source(
-#     source=gitlab_repository_block,
-#     # GitRepository(
-#     # url="https://gitlab.com/saberdy/prefect_demo.git",
-#     # url=gitlab_credentials_block,
-#     # url="https://gitlab.com/api/v4/projects/53809079",
-#     # branch="master",
-#     # credentials={"username": "saberdy",
-#     #             "access_token": gitlab_credentials_block},
-#     # credentials={
-#     #     "username": "saberdy",
-#     #     "access_token": Secret.load("prefect-demo").get(),
-#     #     # "branch": "master"
-#     entrypoint="repo_info.py:get_repo_info"
-#    )
+
 
 if __name__ == "__main__":
     # dummy_flow()
