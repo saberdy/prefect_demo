66dfd17b179e3a4954f0547b9395d0d7f93af238
saber <sabergit@proton.me>
2024-01-22 16:20:37 +0100

    modify the flow code and deploy the flow using .deploy method

--- load_from_storage.py
+++ load_from_storage.py
@@ -1,17 +1,15 @@
 from datetime import datetime
 from prefect import flow
 
+
 @flow(retries=3, retry_delay_seconds=5, log_prints=True)
 def dummy_flow(date: datetime = datetime.now()):
     print(f"It was {date.strftime('%A')} on {date.isoformat()}")
 
 
-my_flow = flow.from_source(
-    source="https://github.com/saberdy/prefect_demo.git",
-    entrypoint="load_from_storage.py:dummy_flow"
-)
-
-
 if __name__ == "__main__":
     # dummy_flow()
-    my_flow()
+    flow.from_source(
+        source="https://github.com/saberdy/prefect_demo.git",
+        entrypoint="load_from_storage.py:dummy_flow"
+    ).deploy(name='dummy_flow_deploy', work_pool_name='my-managed-pool')
